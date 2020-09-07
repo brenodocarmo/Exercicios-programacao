3629ee8bc54bc343c531be8cb57089c2e52f8235
brenodocarmo <brenocarmosousa@gmail.com>
2020-09-07 12:18:54 -0300

    1144 - Sequencia Logica

---
+++ uri-online/1144.py
@@ -0,0 +1,8 @@
+valor = int(input())
+
+i = 1
+
+while i <= valor:
+    print(str(int(i))+" "+str(int(i*i))+" "+str(int(i*i*i)))
+    print(str(int(i))+" "+str(int(i*i+1))+" "+str(int(i*i*i+1)))
+    i += 1
